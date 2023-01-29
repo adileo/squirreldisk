f71a8936251a9117b3f750a6036557235abcc5c3
Adileo Barone <Adileob@gmail.com>
2023-01-29 22:09:13 +0100

    linux fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,27 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { invoke } from "@tauri-apps/api/tauri";
 import { MemoryRouter as Router, Route, Routes } from "react-router-dom";
 import TitleBar from "./TitleBar";
 import { DiskList } from "./DiskList";
 import DiskDetail from "./DiskDetail";
 
+import { platform } from '@tauri-apps/api/os';
+
 function App() {
   const [greetMsg, setGreetMsg] = useState("");
   const [name, setName] = useState("");
-
-  async function greet() {
-    // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
-    setGreetMsg(await invoke("greet", { name }));
-  }
-
+  const [isLinux, setIsLinux] = useState(false);
+  useEffect(()=>{
+    const platformName = platform().then((plat) => {
+      if(plat === 'linux'){
+        setIsLinux(true);
+      }
+    });
+  },[])
   return (
     <Router>
       <div
-        className="flex flex-col justify-items-stretch	items-stretch min-h-full"
+        className={"flex flex-col justify-items-stretch	items-stretch min-h-full"+(isLinux ? " linuxbg":"")}
         style={{ borderRadius: 20 }}
       >
         <TitleBar></TitleBar>
